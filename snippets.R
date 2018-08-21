8631474c4637a3bb27c85799835a7d1834eab7f1
rlavelli <riccardo.lavelli7@gmail.com>
2018-08-21 20:25:05 +0200

    # third commit

--- snippets.R
+++ snippets.R
@@ -1,3 +1,7 @@
 # Useful snippets of code
 
-# wip
+# git remote add origin https://github.com/rlavelli/RUtility.git
+
+# git config remote.origin.url git@github.com:rlavelli/RUtility.git
+# git pull -u origin master
+# git push -u origin master
