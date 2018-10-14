9a596ea8e9fe21c0e079f0b95dc4cdc75c2ea179
rlavelli <riccardo.lavelli7@gmail.com>
2018-10-14 11:21:38 +0200

    Update ts_feat.R

--- ts_feat.R
+++ ts_feat.R
@@ -14,5 +14,13 @@ hwl <- bind_cols(
              s.window = "periodic", robust = T),
   tsfeatures(yahoo, "max_kl_shift", width = 48),
   tsfeatures(yahoo,
-             c("mean", "var"))
-)
+             c("mean", "var"), scale = F, na.rm = T),
+  tsfeatures(yahoo,
+             c("max_level_shift", "max_var_shifyt"), trim = T)) %>%
+  select(mean, var, x_acf1, trend,
+         seasonal_strength, peak, trough,
+         entropy, lumpiness, spike, max_level_shift,
+         max_var_shift, flat_spots, crossing_points,
+         max_kl_shift, time_kl_shift)
+
+pr <- prcomp(na.omit(hwl), scale = T)$x %>% as.tibble()
