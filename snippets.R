aac88e182413868ee89753a04d9547591ee128f5
rlavelli <riccardo.lavelli7@gmail.com>
2018-10-16 15:40:35 +0200

    added possibly() use case

--- snippets.R
+++ snippets.R
@@ -5,3 +5,12 @@
 # git config remote.origin.url git@github.com:rlavelli/RUtility.git
 # git pull -u origin master
 # git push -u origin master
+
+possible_my_function <- possibly(my_function, otherwise = NA_real_)
+numbers_with_error <- list(1, 2, 3, "spam", 4) # wrong data type
+
+map(numbers_with_error, possible_my_function) # executes with NA
+
+
+
+
