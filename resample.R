17869036a330419d2cd8bf2630cc6e8097ff0f5f
rlavelli <riccardo.lavelli7@gmail.com>
2019-02-16 18:28:03 +0100

    Update resample.R
    added second method.

--- resample.R
+++ resample.R
@@ -3,10 +3,19 @@ library(xts)
 #---
 
 # example
+#1
 set.seed(123)
 v <- sample(750)
 t <- seq.POSIXt(from=as.POSIXct("2019-01-01"), 
                 length.out = 750, by="day")
+
+ts <- xts(v, order.by=t)
+
+#2
+set.seed(123)
+v <- sample(10*750)
+t <- seq.POSIXt(from=as.POSIXct("2019-01-01 01:00"), 
+                length.out = 10*750, by="min")
 
 ts <- xts(v, order.by=t)
 
@@ -18,7 +27,9 @@ resample <- function (x, ...) {
   if(length(split_date)==3) {
     class(x) <- c(class(x), "daily")
   }
-  #... if() then...
+  if(length(split_date)>=5) {
+    class(x) <- c(class(x), "minutes")
+  }
   
   UseMethod("resample", x)
 }
@@ -49,7 +60,30 @@ resample.daily <- function(x, to, fun = "mean") {
   return(res)
 }
 
+resample.minutes <- function(x, to, fun="mean") {
+  date <- index(x) #xts
+  FUN <- match.fun(fun) 
+  
+  # to defines the resampling method
+  if (to == "H") {
+    r_index <- strftime(date, format="%Y-%m-%d %H")
+  }
+  
+  # to defines the resampling method
+  if (to == "D") {
+    r_index <- strftime(date, format="%Y-%m-%d")
+  }
+  
+  res <- tibble(time = r_index,
+                values = as.vector(coredata(x))) %>% 
+                                  group_by(time) %>% summarise(values = FUN(values))
+  
+  # returns a tibble
+  return(res)
+}
 
 # example call
+#1
 resample(ts, to="Y", fun="mean")
-
+#2
+resample(ts, to="H", fun="mean")
